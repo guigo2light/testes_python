1b1ac3e14b1b6e2f41156b40e40943a1d418b9ce
guigo2light <gchristovaocn@gmail.com>
2016-08-30 14:24:21 -0300

    novidade...
    agora o leitor está criando um arquivo, e o conteudo linha-a-linha para o novo arquivo.

--- leitor.py
+++ leitor.py
@@ -1,4 +1,6 @@
 veni = open("veni.txt","r")
+novo_veni = open("novo_veni.txt", "a")
 for linha in veni.readlines():
-    print(linha)
+        novo_veni.write(linha)
 veni.close()
+novo_veni.close()
